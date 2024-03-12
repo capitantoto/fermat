45821b321050d0aed33ba76274136b29f31c7404
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2024-03-12 16:23:45 -0300

    Reordenamiento general de archivos

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.in
-git clone https://bitbucket.org/aristas/fermat.git
-pip install -e fermat
-python -m ipykernel install --user --name=fermat
+python -m ipykernel install --user --name=fkdc
